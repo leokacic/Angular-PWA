47acf88599db496c891af753ec6dc795d0631a28
Leo <leobrale@yahoo.com>
2019-03-01 23:48:31 +0100

    promjene

--- jokes/src/app/app.component.ts
+++ jokes/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jokes';
+  title = 'app';
+
+update: boolean =false;
+
+  constructor(updates: SwUpdate){
+    updates.available.subscribe(event=>{
+     //this.update = true;
+
+     updates.activateUpdate().then(() => document.location.reload());
+    })
+  }
 }
--- jokes/src/app/app.module.ts
+++ jokes/src/app/app.module.ts
@@ -6,16 +6,19 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+import { HttpClientModule} from '@angular/common/http';
+import { DataService } from './data.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
