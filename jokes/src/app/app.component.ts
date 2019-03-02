a8de657c4ea590a69eb6d229131be6997b2abbde
Leo <leobrale@yahoo.com>
2019-03-02 14:20:55 +0100

    Final

--- jokes/src/app/app.component.ts
+++ jokes/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,12 +11,18 @@ export class AppComponent {
   title = 'app';
 
 update: boolean =false;
+joke: any;
 
-  constructor(updates: SwUpdate){
+  constructor(updates: SwUpdate,private data: DataService){
     updates.available.subscribe(event=>{
      //this.update = true;
 
      updates.activateUpdate().then(() => document.location.reload());
     })
+  }
+
+  ngOnInit(){
+    this.data.gimmeJokes().subscribe(res=>
+      this.joke = res)
   }
 }
