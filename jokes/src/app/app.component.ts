6a57584976005193a2502e535b75ff57319b47d6
Leo <leobrale@yahoo.com>
2019-03-14 15:33:13 +0100

    prikazuje na sucelju i json u console log

--- jokes/src/app/app.component.ts
+++ jokes/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
-
+import { NgxXml2jsonService } from 'ngx-xml2json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,17 +12,21 @@ export class AppComponent implements OnInit{
 update: boolean =false;
 joke: any;
 
-  constructor(updates: SwUpdate,private service: DataService,){
+  constructor(updates: SwUpdate,private service: DataService,private ngxXml2jsonService: NgxXml2jsonService){
     updates.available.subscribe(event=>{
      //this.update = true;
 
      updates.activateUpdate().then(() => document.location.reload());
     })
+
+    
+
   }
   private errorMessage: string;
   
   private xmlString: string;
   private xml: string;
+  
   ngOnInit(){
     function parseXml(xmlString) {
       return new (<any>window).DOMParser().parseFromString(xmlString, "text/xml");
@@ -30,8 +34,15 @@ joke: any;
     this.service.getXml().subscribe(
       data=> {
         var xml = parseXml(data);
-        alert(this.xml);
+        var xmlDoc = xml;
+
+        document.getElementById("doc").textContent = new XMLSerializer().serializeToString(xmlDoc);
+        document.getElementById("documentElement").textContent = new XMLSerializer().serializeToString(xmlDoc.documentElement);
+
         new XMLSerializer().serializeToString(xml.documentElement);
+
+        const obj = this.ngxXml2jsonService.xmlToJson(xml);
+        console.log(obj);
         console.log("Data",xml)
       },
       error => {
