c6f96f8f7efda151895e9a4b59d392f518887516
Leo <leobrale@yahoo.com>
2019-03-14 15:13:30 +0100

    radi u console logu

--- jokes/src/app/app.component.ts
+++ jokes/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
 
@@ -7,22 +7,38 @@ import { DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
-
 update: boolean =false;
 joke: any;
 
-  constructor(updates: SwUpdate,private data: DataService){
+  constructor(updates: SwUpdate,private service: DataService,){
     updates.available.subscribe(event=>{
      //this.update = true;
 
      updates.activateUpdate().then(() => document.location.reload());
     })
   }
-
+  private errorMessage: string;
+  
+  private xmlString: string;
+  private xml: string;
   ngOnInit(){
-    this.data.gimmeJokes().subscribe(res=>
-      this.joke = res)
+    function parseXml(xmlString) {
+      return new (<any>window).DOMParser().parseFromString(xmlString, "text/xml");
+   }
+    this.service.getXml().subscribe(
+      data=> {
+        var xml = parseXml(data);
+        alert(this.xml);
+        new XMLSerializer().serializeToString(xml.documentElement);
+        console.log("Data",xml)
+      },
+      error => {
+        this.errorMessage = error;
+        console.log("Errror",this.errorMessage)
+      
+      }
+    )
   }
 }
--- jokes/src/app/data.service.ts
+++ jokes/src/app/data.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient,HttpResponse} from '@angular/common/http';
+import { Observable ,of} from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  
+  private baseUrl: string ="http://baltazar.izor.hr/plazepub/kakvoca_prikaz_xml9?p_god=2018&p_filter=&p_ciklus=10&p_zup_id=&p_jezik=&p_grad=&pobjs="
   constructor(private http: HttpClient) { }
 
-  gimmeJokes (){
-    return this.http.get('https://api.chucknorris.io/jokes/random')
+  public getXml(): Observable<any> {
+    console.log("leo");
+    return this.http.get(this.baseUrl,{ responseType: 'text' }).pipe(
+      map(res =>  res),
+      catchError(<T>(error: any, result?: T) => {
+        console.log(error);
+        return of(result as T);
+      })
+    );
   }
+
 }
